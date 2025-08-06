6c8d70bdf866a89acc93d4577ff59fd97f72e238
René Bärnreuther <rene.baernreuther@web.de>
2025-08-06 10:53:03 +0200

    Upgrade to angular 20

--- deplpoy.sh
+++ deplpoy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ng build --configuration=production --base-href="https://rehne93.github.io/imposter-game/" --deploy-url="https://rehne93.github.io/imposter-game/"
+npm run build-prod
 rm -rf docs/
 mkdir docs/
 cp -R ./dist/imposter/browser/* docs/
